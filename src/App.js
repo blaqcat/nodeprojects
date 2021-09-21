feb5414c82ce66df08338944c662bbe3f6527e71
blaqcat <ngakana@protonmail.com>
2021-09-21 20:27:29 +0200

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 import TodoList from './TodoList'
-import React from 'react'
+import React, { useState, useRef} from 'react'
 
 function App() {
+  const [todos, setTodos] =useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: 1, name: name, complete: false}]
+    })
+    console.log(name)
+    todoNameRef.current.value = null
+
+  }
+  
   return (
     <React.Fragment>
-      <TodoList />
-      <input type="text"/>
-      <button>Add ToDo</button>
+      <TodoList todos={todos} />
+      <input ref={todoNameRef} type="text"/>
+      <button onClick={handleAddTodo}>Add ToDo</button>
       <button>Clear Completed</button>
+      <div>0 left to do</div>
 
     </React.Fragment>
   )
